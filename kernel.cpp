92f8d7a2f1c1c6ff07961032933d0e640fc9f1d1
Aashish <aashishsharma.ajm@gmail.com>
2020-07-24 22:27:37 +0530

    Added a gdt.h file which defines memory segments for interrupt Added a types file to define integer types Added a ports.h file that defines the harware config Added a ports.cpp file which will be used to implement hardware support in the future

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,6 @@
 
+#include "types.h"
+#include "gdt.h"
 
 typedef void(*constructor)();
 extern "C" constructor start_ctors;
@@ -12,7 +14,7 @@ extern "C" void callConstructors()
 //pointer that points to the memory address of the display device
 void printf(char* str){
 
-    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
     for(int i = 0 ; str[i]!='\0' ; ++i)
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
@@ -21,9 +23,11 @@ void printf(char* str){
 }
 
 
-extern "C" void kernelMain(void* multiboot_structure , unsigned int magicnumber){
+extern "C" void kernelMain(void* multiboot_structure , uint32_t magicnumber){
 
     printf("This is an attemp to make an operating system -Aashish.");
+
+    GlobalDescriptorTable gdt;
 
     while(1);
 
