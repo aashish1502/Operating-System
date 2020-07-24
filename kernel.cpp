8be26f327d1c0672cda8c96c2704fc9a8fd561b4
Aashish <aashishsharma.ajm@gmail.com>
2020-07-24 23:30:57 +0530

    Made some changes for features to be more prominent

--- kernel.cpp
+++ kernel.cpp
@@ -64,8 +64,9 @@ void printf(char* str)
 
 extern "C" void kernelMain(void* multiboot_structure , uint32_t magicnumber){
 
-    printf("This is an attemp to make an operating system -Aashish.");
-    printf("This is an attemp to make an operating system -Aashish.");
+    printf("This is an attemp to make an operating system -Aashish.\n");
+    printf("This is an attemp to make an operating system -Aashish.\n");
+    printf("!@#$.\n");
 
     GlobalDescriptorTable gdt;
 
