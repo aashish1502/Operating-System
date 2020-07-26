41653d71590b9eb9fe0a0e5a7f3693e50fd5e33c
Aashish <aashishsharma.ajm@gmail.com>
2020-07-26 23:23:48 +0530

    Added additional requests in the Interrupt

--- interrupts.h
+++ interrupts.h
@@ -1,5 +1,5 @@
-#ifndef __INTERRUPTS_H
-#define __INTERRUPS_H
+#ifndef __INTERRUPTMANAGER_H
+#define __INTERRUPMANAGER_H
 
 #include "types.h"
 #include "port.h"
@@ -30,6 +30,8 @@
         }__attribute__((packed));
 
 
+        uint16_t hardwareInterruptOffset;
+        
         static void SetInterruptDescriptorTableEntry(
             uint8_t interruptNumber,
             uint16_t codeSegmentSelectorOffset,
@@ -39,6 +41,50 @@
 
 
         );
+
+        static void InterruptIgnore();
+
+        static void HandleInterruptRequest0x00();
+        static void HandleInterruptRequest0x01();
+        static void HandleInterruptRequest0x02();
+        static void HandleInterruptRequest0x03();
+        static void HandleInterruptRequest0x04();
+        static void HandleInterruptRequest0x05();
+        static void HandleInterruptRequest0x06();
+        static void HandleInterruptRequest0x07();
+        static void HandleInterruptRequest0x08();
+        static void HandleInterruptRequest0x09();
+        static void HandleInterruptRequest0x0A();
+        static void HandleInterruptRequest0x0B();
+        static void HandleInterruptRequest0x0C();
+        static void HandleInterruptRequest0x0D();
+        static void HandleInterruptRequest0x0E();
+        static void HandleInterruptRequest0x0F();
+        static void HandleInterruptRequest0x31();
+
+
+        static void HandleException0x00();
+        static void HandleException0x01();
+        static void HandleException0x02();
+        static void HandleException0x03();
+        static void HandleException0x04();
+        static void HandleException0x05();
+        static void HandleException0x06();
+        static void HandleException0x07();
+        static void HandleException0x08();
+        static void HandleException0x09();
+        static void HandleException0x0A();
+        static void HandleException0x0B();
+        static void HandleException0x0C();
+        static void HandleException0x0D();
+        static void HandleException0x0E();
+        static void HandleException0x0F();
+        static void HandleException0x10();
+        static void HandleException0x11();
+        static void HandleException0x12();
+        static void HandleException0x13();
+
+        static uint32_t HandleInterrupt(uint8_t interrupt, uint32_t esp);
 
 
 
@@ -51,17 +97,11 @@
 
     public:
 
-        InterruptManager(GlobalDescriptorTable* gdt);
+        InterruptManager(uint16_t hardwareInterruptOffset, GlobalDescriptorTable* gdt);
         ~InterruptManager();
-
+        uint16_t HardwareInterruptOffset();
         void Activate();
-
-
-        static uint32_t handleInterrupt(uint8_t interruptNumber, uint32_t esp);
-
-        static void IgnoreInterruptRequest();
-        static void HandleInterruptRequest0x00();
-        static void HandleInterruptRequest0x01();
+        void Deactivate();
     };
 
 #endif
